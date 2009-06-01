31d2e90c0d9a53250e6e2a3ed4e364b02148f2bf
Ale Muñoz <bomberstudios@gmail.com>
2009-06-01 13:23:22 +0200

    Updated sinatra app for fullscreen and placeholder support

--- app.rb
+++ app.rb
@@ -1,16 +1,30 @@
-WIDTH = 800
-HEIGHT = 600
-FPS = 31
-PLAYER = 8
-BGCOLOR = "#ffffff"
-APP = "flvplayer"
 
 require 'rubygems'
 require 'sinatra'
 
+configure do
+  WIDTH = 640
+  HEIGHT = 360
+  FPS = 31
+  PLAYER = 8
+  BGCOLOR = "#000000"
+  APP = "flvplayer"
+end
+
 helpers do
   def swf_helper
-    return "<embed src='/swf' width='#{WIDTH}' height='#{HEIGHT}' type='application/x-shockwave-flash'></embed>"
+    return <<-HTML
+    <script type="text/javascript" src="/swfobject"></script>
+    <div id="flashcontent">
+      This text is replaced by the Flash movie.
+    </div>
+    <script type="text/javascript">
+      var so = new SWFObject("/swf", "mymovie", "#{WIDTH}", "#{HEIGHT}", "8", "#{BGCOLOR}");
+      so.addParam('allowFullScreen',true);
+      so.addVariable('aspect_ratio',16/9);
+      so.write("flashcontent");
+    </script>
+    HTML
   end
 end
 
@@ -24,4 +38,11 @@ get "/" do
 end
 get "/swf" do
   send_file "deploy/#{APP}.swf", :type => 'application/x-shockwave-flash', :disposition => 'inline'
+end
+get '/placeholder' do
+  send_file "assets/placeholder_small.jpg", :disposition => 'inline'
+end
+
+get '/swfobject' do
+  send_file "deploy/swfobject.js", :disposition => 'inline'
 end
