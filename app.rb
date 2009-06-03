8276a456fd3d32524f8ae1aeb62851c367843adb
Ale Muñoz <bomberstudios@gmail.com>
2009-06-03 13:56:14 +0200

    Add pseudostreaming support to sinatra app.

--- app.rb
+++ app.rb
@@ -1,8 +1,39 @@
 require 'rubygems'
 require 'sinatra'
+require 'rack/contrib'
 require 'rdiscount'
 
+# This class is returned to Rack adapter as response.
+# Rack callse "each" on it to get response body
+# and sends individual responses to the client.
+# This way we avoid loading whole file to memory
+class FlvStream
+  def initialize(filename, start_pos)
+    @filename = filename
+    @file = File.new(filename, "rb")
+    @start_pos = start_pos
+    @file.seek(@start_pos)
+  end
+
+  def each
+    if @start_pos > 0
+      yield "FLV\x01\x01\x00\x00\x00\x09\x00\x00\x00\x09" # If we are not starting from beggining
+                                                          # we must prepend FLV header to output
+      @start_pos = 0
+    end
+
+    begin (chunk = @file.read(4*1024)) # Go and experiment with best buffer size for you
+      yield chunk
+    end while chunk.size == 4*1024
+  end
+
+  def length
+    File.size(@filename) - @start_pos
+  end
+end
+
 configure do
+  use Rack::Evil
   WIDTH = 640
   HEIGHT = 360
   FPS = 31
@@ -48,7 +79,9 @@ get '/placeholder' do
   send_file "assets/placeholder.png", :disposition => 'inline'
 end
 get '/video' do
-  send_file "deploy/test_video.flv", :disposition => 'inline'
+  flv = FlvStream.new("deploy/test_video.flv", params[:start].to_i)
+  puts params[:start].to_i
+  throw :response, [200, {'Content-Type' => 'application/x-flv', "Content-Length" => flv.length.to_s}, flv]
 end
 get '/swfobject' do
   send_file "deploy/swfobject.js", :disposition => 'inline'
